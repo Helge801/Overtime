3d48a80ffcb3e6be3c80f35a5ed9e2183f1d931e
Bryan Lund <bryan.helge@gmail.com>
2018-01-02 16:02:15 -0700

    Post creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post + 1} rationale content")
-  p "created post #{post + 1}"
+  puts "created post #{post + 1}"
 end
 
-p '*' * 50
+puts '*' * 50
 puts "   100 Posts have been created"
-p '*' * 50
+puts '*' * 50
