d0e113c1c70e639e0ee6d346abbdfca7aa08a975
Bryan Lund <bryan.helge@gmail.com>
2018-01-03 11:27:28 -0700

    removed rspec work arounds

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,10 +17,10 @@ RSpec.describe User, type: :model do
     end
   end
 
-  # describe "custom name methods" do
-  #   it 'has a full name method that combines first and last name' do
-  #     expect(@user.full_name).to eq("SNOW, JON")
-  #   end
-  # end
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("SNOW, JON")
+    end
+  end
   
 end
