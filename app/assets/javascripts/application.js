9cffe73a80bbf00b8403a78229f7d2e0a8fb2fad
Bryan Lund <bryan.helge@gmail.com>
2018-01-02 18:32:04 -0700

    imported nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+// 
+// 
+// 
+//= require jquery3
 //= require rails-ujs
+//= require jquery-ui
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
