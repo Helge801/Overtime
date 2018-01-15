9537ef614b8445bc7667f22791ea51f7b5049072
Bryan Lund <bryan.helge@gmail.com>
2018-01-15 15:38:47 -0700

    styled show page and added links to show and edit views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
-User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+def em str
+  puts '*' * 50
+    puts "   " + str
+  puts '*' * 50
+end
+
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+em "1 Regular User created."
 
 User.create(email: "goodbye@goodbye.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow", type: "AdminUser")
 
-puts '*' * 50
-puts "1 User created"
-puts '*' * 50
+em "1 Admin User created"
 
 
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: [1,2].sample, overtime_request: 2.5)
   puts "created post #{post + 1}"
 end
 
-puts '*' * 50
-puts "   100 Posts have been created"
-puts '*' * 50
+em "100 Posts have been created"
