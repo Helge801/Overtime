c680a1f4b37c803428c0adf25b1ff6f1e990eb32
Bryan Lund <bryan.helge@gmail.com>
2018-01-23 13:35:21 -0700

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ em "1 Admin User created"
 
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: [1,2].sample, overtime_request: 2.5, status: [1,0].sample)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: [1,2].sample, overtime_request: 2.5, status: [1,0].sample)
   puts "created post #{post + 1}"
 end
 
