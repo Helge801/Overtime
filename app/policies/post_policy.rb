7ff90ceaf877a61d7f6491243a18e5070aaac9a3
Bryan Lund <bryan.helge@gmail.com>
2018-01-24 15:21:20 -0700

    implimented functial buttons for employee home pae comfirmations

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,15 +1,14 @@
 class PostPolicy < ApplicationPolicy
-
   def update?
     return true if post_approved? && admin?
     return true if user_or_admin && !post_approved?
   end
 
-  private
+  def approve?
+    admin?
+  end
 
-    def approve
-      admin?
-    end
+  private
 
     def user_or_admin
       record.user_id == user.id || admin?
@@ -22,5 +21,4 @@ class PostPolicy < ApplicationPolicy
     def post_approved?
       record.approved?
     end
-
 end
