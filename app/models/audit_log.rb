b4699de81cf0780e3fe16cd91fc4c0753fcd4cb8
Bryan Lund <bryan.helge@gmail.com>
2018-01-25 14:06:38 -0700

    added end date to audit logs apon approval

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,11 +1,15 @@
 class AuditLog < ApplicationRecord
   enum status: { pending: 0, confirmed: 1}
   belongs_to :user
-
+  before_update :set_end_date, if: :confirmed?
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_defaults
 
   private
+
+  def set_end_date
+    self.end_date = Date.today
+  end
 
   def set_defaults
     self.start_date ||= Date.today - 6.days
